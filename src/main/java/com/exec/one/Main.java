69e8770cc7efef0f9385fbcd3aede8ce709a4ed8
Miro Kopecky <kopeckymiroslav@gmail.com>
2016-08-19 11:19:49 +0200

    main method calls a fake services compile: $javac -cp ./src/main/java ./src/main/java/com/exec/one/*.java ./src/main/java/com/exec/one/**/*.java -d ./out/

--- src/main/java/com/exec/one/Main.java
+++ src/main/java/com/exec/one/Main.java
@@ -1,7 +1,11 @@
 package com.exec.one;
 
+import com.exec.one.service.MagicService;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Main Class Start");
+        MagicService service = new MagicService();
+        System.out.println("MESSAGE : " + service.getMessage());
     }
 }
---
+++ src/main/java/com/exec/one/service/MagicService.java
@@ -0,0 +1,14 @@
+package com.exec.one.service;
+
+public class MagicService {
+
+    private final String message;
+    public MagicService(){
+        this.message = "Magic Message";
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+}
